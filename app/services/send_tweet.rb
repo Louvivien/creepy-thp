8f59b69f3ac589418e4d1536bea757ee1410579e
Justine <>
2018-02-22 14:22:11 +0100

    add services

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -1,12 +1,9 @@
 class SendTweet
-	#def initialize(tweet)
-	#	@tweet = tweet
-	#end
-
 	def perform
 		log_in_to_twitter
 		#tweet_universities
 		#tweet_students
+		testing
 	end
 
 	def log_in_to_twitter
@@ -23,7 +20,7 @@ class SendTweet
 	end
 
 	def tweet_universities
-		@universities = ["@USPC_news", "@SorbonneParis1", "@UnivParisSaclay", "@UPECactus", "@univamu", "@univpaulvalery", "@AssasParis2", "@CPUniversite", "@univ_paris13", "@SorbonneParis3", "@UParisDescartes", "@UnivParis8", "@ParisDiderot", "@UQAM", "@UParisNanterre"‏, "@Paris_Dauphine", "@UnivNantes", "@unilim", "@UBO_UnivBrest", "@UnivLaRochelle", "@UnivCorse", "@Univ_Lorraine", "@Univ_St_Etienne", "@upemlv", "@UnivLyon1", "@univ_lyon2", "@UniversiteLyon", "@UnivRennes_2", "@ucly", "@UnivEvry", "@universitereims"‏, "@ScienceSorbonne", "@univpaulvalery", "@FacLibredeDroit‏", "@Universite_Caen‏"‏‏‏]
+		@universities = ["@USPC_news","@SorbonneParis1","@UnivParisSaclay","@UPECactus","@univamu","@univpaulvalery","@AssasParis2","@CPUniversite","@univ_paris13","@SorbonneParis3","@UParisDescartes","@UnivParis8","@ParisDiderot","@UQAM","@UParisNanterre"‏,"@Paris_Dauphine","@UnivNantes","@unilim","@UBO_UnivBrest","@UnivLaRochelle","@UnivCorse","@Univ_Lorraine","@Univ_St_Etienne","@upemlv","@UnivLyon1","@univ_lyon2","@UniversiteLyon","@UnivRennes_2","@ucly","@UnivEvry","@universitereims"‏,"@ScienceSorbonne","@univpaulvalery","@FacLibredeDroit‏","@Universite_Caen‏"‏‏‏]
 		@universities.each do |university|
 			send_tweet("#{university}, vous connaissez The Hacking Project ? La seule formation de code gratuite pour vos étudiants. Pour la découvrir, c'est par ici : http://bit.ly/2EXYivy")
 			sleep(30)
@@ -40,4 +37,9 @@ class SendTweet
 			end
 		end
 	end
+
+	def testing
+		@client.user_search('Université')
+	end
+
 end
