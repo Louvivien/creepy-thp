e6d698fd887e0135e0918b8c6263fb1fce518d9d
Justine <>
2018-02-22 13:53:17 +0100

    twitter bot to tweet students and universities

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -1,12 +1,12 @@
 class SendTweet
-	def initialize(tweet)
-		@tweet = tweet
-	end
+	#def initialize(tweet)
+	#	@tweet = tweet
+	#end
 
 	def perform
 		log_in_to_twitter
-		#send_tweet(@tweet)
-		tweet_universities
+		#tweet_universities
+		#tweet_students
 	end
 
 	def log_in_to_twitter
@@ -23,9 +23,21 @@ class SendTweet
 	end
 
 	def tweet_universities
-		universities = []
-		universities << @client.user("université")
-		#universities.each {|university| @client.update(tweet)}
+		@universities = ["@USPC_news", "@SorbonneParis1", "@UnivParisSaclay", "@UPECactus", "@univamu", "@univpaulvalery", "@AssasParis2", "@CPUniversite", "@univ_paris13", "@SorbonneParis3", "@UParisDescartes", "@UnivParis8", "@ParisDiderot", "@UQAM", "@UParisNanterre"‏, "@Paris_Dauphine", "@UnivNantes", "@unilim", "@UBO_UnivBrest", "@UnivLaRochelle", "@UnivCorse", "@Univ_Lorraine", "@Univ_St_Etienne", "@upemlv", "@UnivLyon1", "@univ_lyon2", "@UniversiteLyon", "@UnivRennes_2", "@ucly", "@UnivEvry", "@universitereims"‏, "@ScienceSorbonne", "@univpaulvalery", "@FacLibredeDroit‏", "@Universite_Caen‏"‏‏‏]
+		@universities.each do |university|
+			send_tweet("#{university}, vous connaissez The Hacking Project ? La seule formation de code gratuite pour vos étudiants. Pour la découvrir, c'est par ici : http://bit.ly/2EXYivy")
+			sleep(30)
+		end
 	end
 
+	def tweet_students
+		@universities.each do |university|
+			puts university
+			@client.followers(university).each do |follower|
+				puts follower.screen_name
+				send_tweet("@#{follower.screen_name}, vous connaissez The Hacking Project ? La seule formation de code gratuite à combiner avec vos études. Pour la découvrir, c'est par ici : http://bit.ly/2GxzQOT")
+				sleep(30)
+			end
+		end
+	end
 end
