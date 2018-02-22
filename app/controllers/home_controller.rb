283672385cdaa43f6d6b5c0e84e14fb9f272f926
Justine <>
2018-02-22 11:06:07 +0100

    added new metods for specifi landing pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def students
+  end
+
+  def schools
+  end
+
+  def journalists
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/etudiants', to: 'home#students'
+  get '/ecoles', to: 'home#schools'
+  get '/journalistes', to: 'home#journalists'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
