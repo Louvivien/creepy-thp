31a8e0a08e6e52e9c35a0169e7e8f982de95104b
Justine <>
2018-02-23 01:15:56 +0100

    changed html in new landings

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -41,9 +41,7 @@ class SendTweet
 
 	def tweet_students
 		@universities.each do |university|
-			puts university
 			@client.followers(university).each do |follower|
-				puts follower.screen_name
 				send_tweet("@#{follower.screen_name}, vous connaissez The Hacking Project ? La seule formation de code gratuite à combiner avec vos études. Pour la découvrir, c'est par ici : http://bit.ly/2GxzQOT")
 				sleep(30)
 			end
